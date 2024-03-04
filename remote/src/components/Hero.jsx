7231835b30f5532d2fe6d1230c6cc57160b310bb
Gustavo Benitez <gustavo.benitez.developer@gmail.com>
2024-03-04 18:58:20 -0300

    exponer-hero-desde-remote

---
+++ remote/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+export const Hero = () => {
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src="/react.svg" className="logo" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+    </>
+  );
+};
+
+export default Hero;
